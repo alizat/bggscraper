e527d96c9a41f48b5bcea81823acf556f701b1d2
Ali Ezzat <ali_ezzat85@yahoo.com>
2022-12-29 01:01:24 +0200

    update fn: get_user_collection(), uses bgg XML api now

--- R/get_user_collection.R
+++ R/get_user_collection.R
@@ -1,36 +1,89 @@
 get_user_collection <- function(user) {
     # user collection link
-    link <- paste0('https://boardgamegeek.com/collection/user/', user)
+    link <- paste0('https://boardgamegeek.com/xmlapi2/collection?username=', user)
 
     # obtain html page
     html_page <- rvest::read_html(link)
 
-    # grab user collection table
-    collectionitems <- rvest::html_table(rvest::html_elements(html_page, xpath = '//*[@id="collectionitems"]'))
-    collectionitems <- collectionitems[[1]]
-    collectionitems <- dplyr::mutate_if(collectionitems, is.character, stringr::str_squish)
-    collectionitems <- dplyr::select(collectionitems, Title, UserRating, Status)
+    # features to extract
+    features <-
+        list(
+            'item::objecttype' = '',
+            'item::objectid' = '',
+            'item::subtype' = '',
+            'name' = '',
+            'yearpublished' = '',
+            'stats::minplayers' = '',
+            'stats::maxplayers' = '',
+            'stats::minplaytime' = '',
+            'stats::maxplaytime' = '',
+            'stats::playingtime' = '',
+            'stats::numowned' = '',
+            'rating::value' = '',
+            'rating > usersrated::value' = '',
+            'rating > average::value' = '',
+            'rating > bayesaverage::value' = '',
+            'rating > stddev::value' = '',
+            'rating > median::value' = '',
+            'status::own' = '',
+            'status::prevowned' = '',
+            'status::fortrade' = '',
+            'status::want' = '',
+            'status::wanttoplay' = '',
+            'status::wanttobuy' = '',
+            'status::wishlist' = '',
+            'status::preordered' = '',
+            'status::lastmodified' = '',
+            'numplays' = ''
+        )
 
-    # user rating column (separate rating from date that rating was made)
-    collectionitems$UserRatingDate <- purrr::map_chr(collectionitems$UserRating, ~ paste(unlist(stringr::str_split(.x, ' '))[-1], collapse = ' ') )
-    collectionitems$UserRating     <- purrr::map_chr(collectionitems$UserRating, ~       unlist(stringr::str_split(.x, ' '))[[1]] )
-
-    # missing rating to be NA instead of ''
-    collectionitems$UserRating[collectionitems$UserRating == 'N/A']      <- NA
-    collectionitems$UserRatingDate[collectionitems$UserRatingDate == ''] <- NA
-
-    # status
-    statuses <- c('Owned', 'Want To Buy', 'Want To Play')
-    for (stts in statuses) {
-        collectionitems[[stts]] <- stringr::str_detect(collectionitems$Status, stts)
+    # extract features
+    for (feat in names(features)) {
+        if (stringr::str_detect(feat, '::')) {
+            components <- unlist(stringr::str_split(feat, '::'))
+            tag <- components[[1]]
+            attribute <- components[[2]]
+            features[[feat]] <- rvest::html_elements(html_page, tag)
+            features[[feat]] <- as.character(rvest::html_attr(features[[feat]], attribute))
+        } else {
+            features[[feat]] <- rvest::html_elements(html_page, feat)
+            features[[feat]] <- rvest::html_text(features[[feat]])
+        }
     }
-    collectionitems <- dplyr::select(collectionitems, -Status)
 
-    # game titles & links
-    game_links  <- rvest::html_attr(rvest::html_elements(html_page, 'a.primary'), 'href')
-    game_links  <- paste0('https://boardgamegeek.com', game_links)
-    collectionitems <- dplyr::mutate(collectionitems, game_link = game_links)
+    # adjust features names
+    names(features)[names(features) == 'item::objecttype']             <- 'object_type'
+    names(features)[names(features) == 'item::objectid']               <- 'object_id'
+    names(features)[names(features) == 'item::subtype']                <- 'sub_type'
+    names(features)[names(features) == 'yearpublished']                <- 'year_published'
+    names(features)[names(features) == 'stats::minplayers']            <- 'min_players'
+    names(features)[names(features) == 'stats::maxplayers']            <- 'max_players'
+    names(features)[names(features) == 'stats::minplaytime']           <- 'min_play_time'
+    names(features)[names(features) == 'stats::maxplaytime']           <- 'max_play_time'
+    names(features)[names(features) == 'stats::playingtime']           <- 'playing_time'
+    names(features)[names(features) == 'stats::numowned']              <- 'num_owned'
+    names(features)[names(features) == 'rating::value']                <- 'usr_rating'
+    names(features)[names(features) == 'rating > usersrated::value']   <- 'rating_users_rated'
+    names(features)[names(features) == 'rating > average::value']      <- 'rating_avg'
+    names(features)[names(features) == 'rating > bayesaverage::value'] <- 'rating_bayes_avg'
+    names(features)[names(features) == 'rating > stddev::value']       <- 'rating_stddev'
+    names(features)[names(features) == 'rating > median::value']       <- 'rating_median'
+    names(features)[names(features) == 'status::own']                  <- 'owned'
+    names(features)[names(features) == 'status::prevowned']            <- 'prev_owned'
+    names(features)[names(features) == 'status::fortrade']             <- 'for_trade'
+    names(features)[names(features) == 'status::want']                 <- 'want'
+    names(features)[names(features) == 'status::wanttoplay']           <- 'want_to_play'
+    names(features)[names(features) == 'status::wanttobuy']            <- 'want_to_buy'
+    names(features)[names(features) == 'status::wishlist']             <- 'wishlist'
+    names(features)[names(features) == 'status::preordered']           <- 'preordered'
+    names(features)[names(features) == 'status::lastmodified']         <- 'last_modified'
+    names(features)[names(features) == 'numplays']                     <- 'num_plays'
 
-    # return
+    # convert to data frame
+    collectionitems <- dplyr::bind_cols(features)
+    collectionitems <- dplyr::mutate(collectionitems, user = user)
+    collectionitems <- dplyr::select(collectionitems, user, dplyr::everything())
+
+    # return collection
     collectionitems
 }
--- R/grab_game_info.R
+++ R/grab_game_info.R
@@ -1,8 +1,4 @@
-grab_game_info <- function(game_link) {
-    # game id
-    game_id <- stringr::str_extract(game_link, '/[:digit:]+/')
-    game_id <- stringr::str_replace_all(game_id, '/', '')
-
+grab_game_info <- function(game_id) {
     # game details
     game_xml_link <- paste0('https://www.boardgamegeek.com/xmlapi2/thing?id=', game_id, '&stats=1')
     game_details <- rvest::read_html(game_xml_link)
@@ -15,9 +11,9 @@ grab_game_info <- function(game_link) {
             'description' = '',
             'minplayers' = '',
             'maxplayers' = '',
-            'playingtime' = '',
             'minplaytime' = '',
             'maxplaytime' = '',
+            'playingtime' = '',
             'minage' = '',
             'link[type=boardgamecategory]' = '',
             'link[type=boardgamemechanic]' = '',
@@ -51,12 +47,12 @@ grab_game_info <- function(game_link) {
 
     # adjust features names
     names(features)[names(features) == 'name[type=primary]']            <- 'name'
-    names(features)[names(features) == 'yearpublished']                 <- 'yearpublished'
+    names(features)[names(features) == 'yearpublished']                 <- 'year_published'
     names(features)[names(features) == 'minplayers']                    <- 'min_players'
     names(features)[names(features) == 'maxplayers']                    <- 'max_players'
-    names(features)[names(features) == 'playingtime']                   <- 'playing_time'
     names(features)[names(features) == 'minplaytime']                   <- 'min_playtime'
     names(features)[names(features) == 'maxplaytime']                   <- 'max_playtime'
+    names(features)[names(features) == 'playingtime']                   <- 'playing_time'
     names(features)[names(features) == 'minage']                        <- 'min_age'
     names(features)[names(features) == 'link[type=boardgamecategory]']  <- 'category'
     names(features)[names(features) == 'link[type=boardgamemechanic]']  <- 'mechanic'
@@ -64,9 +60,9 @@ grab_game_info <- function(game_link) {
     names(features)[names(features) == 'link[type=boardgamedesigner]']  <- 'designer'
     names(features)[names(features) == 'link[type=boardgameartist]']    <- 'artist'
     names(features)[names(features) == 'link[type=boardgamepublisher]'] <- 'publisher'
-    names(features)[names(features) == 'ratings > usersrated']          <- 'users_rated'
-    names(features)[names(features) == 'ratings > average']             <- 'avg_rating'
-    names(features)[names(features) == 'ratings > bayesaverage']        <- 'bayes_avg_rating'
+    names(features)[names(features) == 'ratings > usersrated']          <- 'rating_users_rated'
+    names(features)[names(features) == 'ratings > average']             <- 'rating_avg'
+    names(features)[names(features) == 'ratings > bayesaverage']        <- 'rating_bayes_avg'
     names(features)[names(features) == 'ratings > ranks > rank[name="boardgame"]'] <- 'rank'
     names(features)[names(features) == 'ratings > owned']               <- 'owned'
     names(features)[names(features) == 'ratings > trading']             <- 'trading'
@@ -76,12 +72,10 @@ grab_game_info <- function(game_link) {
     names(features)[names(features) == 'ratings > numweights']          <- 'num_weights'
     names(features)[names(features) == 'ratings > averageweight']       <- 'avg_weight'
 
-
     # convert to data frame
     features <- purrr::map2_dfr(names(features), features, ~ tibble::tibble(feature = .x, value = .y))
     features <- dplyr::mutate(features, game_id = game_id)
     features <- dplyr::select(features, game_id, feature, value)
-
 
     # return features
     features
