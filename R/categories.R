085f1412a9f2ee12e7a878656ef52cb4e896f399
Ali Ezzat <ali_ezzat85@yahoo.com>
2023-01-10 20:20:47 +0200

    add fn: categories()

---
+++ R/categories.R
@@ -0,0 +1,13 @@
+categories <- function() {
+    # categories link
+    link <- 'https://boardgamegeek.com/browse/boardgamecategory'
+
+    # obtain html page
+    page <- rvest::read_html(link)
+
+    # retrieve categories
+    categories <- rvest::html_text(rvest::html_elements(page, '.forum_table tr > td > a'))
+
+    # return categories
+    categories
+}
