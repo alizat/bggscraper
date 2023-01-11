3a93b778ac41e33ea728c73c7c9fe9a530c9052b
Ali Ezzat <ali_ezzat85@yahoo.com>
2023-01-11 22:20:32 +0200

    add fn: mechanics()

---
+++ R/mechanics.R
@@ -0,0 +1,13 @@
+mechanics <- function() {
+    # mechanics link
+    link <- 'https://boardgamegeek.com/browse/boardgamemechanic'
+
+    # obtain html page
+    page <- rvest::read_html(link)
+
+    # retrieve mechanics
+    mechanics <- rvest::html_text(rvest::html_elements(page, '.forum_table tr > td > a'))
+
+    # return mechanics
+    mechanics
+}
