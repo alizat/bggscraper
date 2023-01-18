c0dea9f306630951da84691d588d887b4b7f6e79
Ali Ezzat <ali_ezzat85@yahoo.com>
2023-01-18 21:51:03 +0200

    add fn: designers()

---
+++ R/designers.R
@@ -0,0 +1,38 @@
+designers <- function(wait = 5) {
+    # initialize
+    designers <- c()
+
+    # get last page index
+    paginator <- rvest::read_html('https://boardgamegeek.com/browse/boardgamedesigner')
+    paginator <- rvest::html_elements(paginator, '.fr a')
+    last_page_element <- paginator[rvest::html_attr(paginator, 'title') == 'last page']
+    last_page <- rvest::html_text(last_page_element)
+    last_page <- stringr::str_extract(last_page, '[:digit:]+')
+    last_page <- as.numeric(last_page)
+
+    # loop on pages
+    for (i in 1:last_page) {
+        print(i)
+
+        # retrieve page i
+        page_i <- glue::glue('https://boardgamegeek.com/browse/boardgamedesigner/page/{i}')
+        page_i <- rvest::read_html(page_i)
+
+        # grab designers of page i
+        designers_i <- rvest::html_text(rvest::html_elements(page_i, 'table > tr > td > a'))
+        if (length(designers_i) == 0)
+            break
+
+        # append
+        designers <- c(designers, designers_i)
+
+        # duration to sleep so BGG website would not block us
+        Sys.sleep(wait)
+    }
+
+    #return designers
+    designers
+}
+
+# TODO: add note that the above code fails to retrieve pages beyond page 20
+#     `rvest::read_html(page_i)` returns empty page when i > 20
