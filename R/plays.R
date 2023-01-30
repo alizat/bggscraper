e7b9106aeb36162e7172ebca048538b4e8fae389
Ali Ezzat <ali_ezzat85@yahoo.com>
2023-01-30 12:39:55 +0200

    plays(): params: type, mindate, maxdate, subtype

--- R/plays.R
+++ R/plays.R
@@ -1,13 +1,25 @@
-plays <- function(game_id = NULL,
-                  user = NULL,
+plays <- function(item_id = NULL,
+                  username = NULL,
+                  type = NULL,
+                  mindate = NULL,
+                  maxdate = NULL,
+                  subtype = NULL,
                   wait = 5) {
-    # check that game id and/or user was supplied
-    assertthat::assert_that(!is.null(game_id) | !is.null(user))
+    # check that item id and/or user name was supplied
+    assertthat::assert_that(!is.null(item_id) | !is.null(username))
 
     # xml link
-    game_id <- if (is.null(game_id)) '' else game_id
-    user    <- if (is.null(user))    '' else user
-    link_base <- glue::glue('https://boardgamegeek.com/xmlapi2/plays?id={game_id}&username={user}')
+    item_id  <- if (is.null(item_id))  '' else item_id
+    username <- if (is.null(username)) '' else username
+    link_base <- glue::glue('https://boardgamegeek.com/xmlapi2/plays?id={item_id}&username={username}')
+    if (!is.null(type))
+        link_base <- paste0(link_base, '&type=', type)
+    if (!is.null(mindate))
+        link_base <- paste0(link_base, '&mindate=', lubridate::as_date(mindate))
+    if (!is.null(maxdate))
+        link_base <- paste0(link_base, '&maxdate=', lubridate::as_date(maxdate))
+    if (!is.null(subtype))
+        link_base <- paste0(link_base, '&subtype=', subtype)
 
     # number of pages
     page <- rvest::read_html(link_base)
@@ -20,7 +32,7 @@ plays <- function(game_id = NULL,
     for (i in 1:num_pages) {
         # retrieve page i
         page_i <- glue::glue('{link_base}&page={i}')
-        page_i <- rvest::read_html(page_i)
+        page_i <- rvest::html_elements(rvest::read_html(page_i), 'plays')
 
         # get plays of page i
         plays_i <- rvest::html_elements(page_i, 'play')
@@ -33,10 +45,10 @@ plays <- function(game_id = NULL,
         # parse
         plays_i <-
             dplyr::tibble(
-                id         = rvest::html_attr(plays_i, 'id'),
-                user       = if (user == '')    rvest::html_attr(plays_i, 'userid')   else user,
-                game_id    = rvest::html_attr(items_i, 'objectid'),
-                game_name  = rvest::html_attr(items_i, 'name'),
+                play_id    = rvest::html_attr(plays_i, 'id'),
+                user_id    = if (username == '') rvest::html_attr(plays_i, 'userid') else rvest::html_attr(page_i, 'userid'),
+                item_id    = rvest::html_attr(items_i, 'objectid'),
+                item_name  = rvest::html_attr(items_i, 'name'),
                 date       = rvest::html_attr(plays_i, 'date'),
                 quantity   = rvest::html_attr(plays_i, 'quantity'),
                 length     = rvest::html_attr(plays_i, 'length'),
