41d8fb84d0e099ec7c7cb5764d8df059794e0391
Ali Ezzat <ali_ezzat85@yahoo.com>
2024-10-08 01:07:31 +0300

    leftovers from the past

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' BGG families
+#'
+#' Families of board games at BGG.
+#'
+#' @format ## `families`
+#' A character vector of board game families
+#' @source <https://www.who.int/teams/global-tuberculosis-programme/data>
+"families"
